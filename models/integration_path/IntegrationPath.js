43c97e02241885ead80eeb486c5080423e492ad4
Yunus Gürlek <ygurlek22@my.uaa.k12.tr>
2022-05-25 18:18:53 +0300

    -

--- models/integration_path/IntegrationPath.js
+++ models/integration_path/IntegrationPath.js
@@ -83,7 +83,7 @@ IntegrationPathSchema.statics.findIntegrationPathsByCompanyIdAndPath = function 
 
   IntegrationPath.find({
     company_id: mongoose.Types.ObjectId(data.company_id.toString()),
-    path: { $regex: `/^${data.path.toLowerCase().split('?')[0].trim()}/` }
+    path: data.path.toLowerCase().split('?')[0].trim()
   }, (err, integration_paths) => {
     if (err) return callback('database_error');
     
